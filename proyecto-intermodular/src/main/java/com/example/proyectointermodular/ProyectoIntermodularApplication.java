595ccac84cb276510243247742d8e2cf81c7bc7a
naeeris <noeliacauqui25@gmail.com>
2025-03-12 22:01:43 +0100

    Modificación nombre BD - Creación repository y controller

--- proyecto-intermodular/src/main/java/com/example/proyectointermodular/ProyectoIntermodularApplication.java
+++ proyecto-intermodular/src/main/java/com/example/proyectointermodular/ProyectoIntermodularApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProyectoIntermodularApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ProyectoIntermodularApplication.class, args);
+        System.out.println("Aplicación Arrancada");
     }
 
 }
--- proyecto-intermodular/src/main/java/com/example/proyectointermodular/repository/InvoiceRepository.java
+++ proyecto-intermodular/src/main/java/com/example/proyectointermodular/repository/InvoiceRepository.java
@@ -9,4 +9,5 @@ public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
 
 
 
+
 }
